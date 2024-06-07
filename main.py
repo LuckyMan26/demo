46483161a0b46144cdc7c8ddff7181555191e41a
LuckyMan26 <volikartem6@gmail.com>
2024-06-07 20:14:05 +0300

    Fix

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import dspy  # Ensure this is the library where your RAG class is defined
 from langchain.vectorstores import Chroma
