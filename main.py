04b2e955778f9db148406f559e8ab650510b5d9e
LuckyMan26 <volikartem6@gmail.com>
2024-06-07 20:21:49 +0300

    Fix

--- main.py
+++ main.py
@@ -17,11 +17,8 @@ from chromadb.config import Settings
 client = chromadb.Client(Settings(persist_directory="db/"))
 collection = client.get_or_create_collection("profile_summarization")
 model = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-langchain_chroma = Chroma(
-    client=client,
-    collection_name="profile_summarization",
-    embedding_function=model,persist_directory="db/"
-)
+langchain_chroma = Chroma(persist_directory="db/", embedding_function=model)
+
 
 lm = dspy.GROQ(model='mixtral-8x7b-32768', api_key ="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",max_tokens=1000 )
 dspy.configure(lm=lm)
